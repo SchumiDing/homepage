e9a0cfb1c631dbc98eb6dea30275ce4c4b90d7eb
SchumiDing <dingruiyi@163.com>
2025-07-17 13:23:12 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export',
   // 加载到 GitHub Pages 子路径下
   basePath: '/homepage',
-  assetPrefix: '/homepage',
+  assetPrefix: '/homepage/',
+  // 导出时在路径末尾加斜杠，生成静态目录结构
+  trailingSlash: true,
   /* config options here */
 };
 
