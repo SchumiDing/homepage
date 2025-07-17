3a60997ce59956d9428e53f0a1e6cf2ec8ef0a79
SchumiDing <dingruiyi@163.com>
2025-07-17 13:14:13 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // 加载到 GitHub Pages 子路径下
+  basePath: '/homepage',
+  assetPrefix: '/homepage',
   /* config options here */
 };
 
